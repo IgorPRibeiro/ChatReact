a965be5883338db1b4e1eefa26b1636602687a8d
IgorPRibeiro <52721517+IgorPRibeiro@users.noreply.github.com>
2020-11-23 03:58:19 -0300

    Retirei minhas informações do connections.js

--- src/connections.js
+++ src/connections.js
@@ -5,13 +5,7 @@ import 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrx4ZD2a3OKg1OMYbrc2JcKtfRU12Reow",
-  authDomain: "sprechen-e44ff.firebaseapp.com",
-  databaseURL: "https://sprechen-e44ff.firebaseio.com",
-  projectId: "sprechen-e44ff",
-  storageBucket: "sprechen-e44ff.appspot.com",
-  messagingSenderId: "750523457520",
-  appId: "1:750523457520:web:61002161d9047648b14735",
+  //Your config
 }
 
 
